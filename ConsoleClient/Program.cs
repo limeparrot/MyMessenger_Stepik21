3aa3d9adedc0ee600b4e721def29f675c540170d
limeparrot <georgpgu@mail.ru>
2021-12-24 20:01:35 +0300

    Добавил класс Message

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace MyMessenger_Stepik21
+namespace MyMessanger_Stepik21
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Message msg = new Message();
             Console.WriteLine("Let's Begin!");
+            Console.Write(msg.ToString());
         }
     }
 }
